2b388cd0c37e79907ed36fa0f13239f9eddccf66
João Albergaria <jvalbergariabar@gmail.com>
2025-09-19 11:15:49 -0300

    Cria Teletransporte e saída + plot por valor de Z  + download da matriz em arquivo .npy

--- Python/plot.py
+++ Python/plot.py
@@ -3,11 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.widgets import Slider
 
-
-# Constants
 P = 100
-T = 50
+T = 150
 S = 200
 N = 10
 
@@ -27,44 +26,106 @@ def preencher():
                 else:
                     score = random.randint(min_val, max_val)
                     labirinto[i][j][k] = score
-                print(labirinto[i][j][k], end="\t")
-            print()
-        print("\n\n")
+                #print(labirinto[i][j][k], end="\t")
+            # print()
+        # print("\n\n")
 
 preencher()
 
 
-# Assuming `labirinto` is already filled
+
+sPos = False
+
+while sPos == False:
+    xS = random.randint(1, N-2)
+    yS = random.randint(1, N-2)
+    zS = random.randint(1, N-2)
+
+    if (labirinto[xS - 1 % N][yS][zS] < P and labirinto[xS + 1 % N][yS][zS] < P and
+        labirinto[xS][yS - 1 % N][zS] < P and labirinto[xS][yS + 1 % N][zS] < P and 
+        labirinto[xS][yS][zS - 1 % N] < P and labirinto[xS][yS][zS + 1 % N] < P):
+        
+        labirinto[xS][yS][zS] = S
+        sPos = True    
+                
+
+nT = random.randint(1, 2)
+for i in range (nT):
+    tPos = False
+    while tPos == False:
+        xT = random.randint(1, N-2)
+        yT = random.randint(1, N-2)
+        zT = random.randint(1, N-2)
+
+        if (labirinto[xT - 1][yT][zT] < P and labirinto[xT + 1 % N][yT][zT] < P and
+            labirinto[xT][yT - 1][zT] < P and labirinto[xT][yT + 1 % N][zT] < P and 
+            labirinto[xT][yT][zT - 1] < P and labirinto[xT][yT][zT + 1 % N] < P and
+            labirinto[xT][yT][zT] != S):
+            
+            labirinto[xT][yT][zT] = T
+            tPos = True    
+
+# Assuming labirinto is already filled
 N = labirinto.shape[0]
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Create coordinates
-x, y, z = np.indices((N+1, N+1, N+1))  # +1 for voxel plotting
-
-# Normalize the values between -5 and 5 for color intensity
+# Normalization for blue values
 norm = colors.Normalize(vmin=-5, vmax=5)
-cmap = plt.cm.Blues  # Blue colormap
+cmap = plt.cm.Blues
 
-# Create facecolors array
-facecolors = np.empty(labirinto.shape, dtype=object)
+# Function to build the voxel plot for a given slice depth
+def build_plot(ax, max_k):
+    ax.clear()
+    facecolors = np.empty(labirinto.shape, dtype=object)
+    filled = np.zeros(labirinto.shape, dtype=bool)
 
-for i in range(N):
-    for j in range(N):
-        for k in range(N):
-            val = labirinto[i, j, k]
-            if val == 100:  # Special P blocks
-                facecolors[i, j, k] = 'red'
-            else:
-                # Map -5..5 to 0..1 for colormap
-                facecolors[i, j, k] = cmap(norm(val))
+    for i in range(N):
+        for j in range(N):
+            for k in range(max_k + 1):  # only show up to current slice
+                val = labirinto[i, j, k]
+                filled[i, j, k] = True
+                if val == P:  # special P blocks
+                    facecolors[i, j, k] = (1, 0, 0, 1.0)  # red, fully opaque
+                elif val == S:
+                    facecolors[i, j, k] = (0, 1, 0, 1.0)
+                elif val == T:
+                    facecolors[i, j, k] = (1, 1, 0, 1.0)
+                else:
+                    rgba = cmap(norm(val))  # (r,g,b,a)
+                    facecolors[i, j, k] = (rgba[0], rgba[1], rgba[2], 0.6)  # semi-transparent blue
 
-# Plot voxels
-ax.voxels(np.ones_like(labirinto, dtype=bool), facecolors=facecolors, edgecolor='k')
+    ax.voxels(filled, facecolors=facecolors, edgecolor='k')
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    ax.set_title(f"Showing layers up to Z={max_k}")
+
+# --- Setup figure ---
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection="3d")
+plt.subplots_adjust(bottom=0.25)
+
+# Initial plot
+build_plot(ax, 0)
+
+# Slider
+ax_slider = plt.axes([0.2, 0.1, 0.6, 0.03])
+slider = Slider(ax_slider, "Z-max", 0, N-1, valinit=0, valstep=1)
+
+def update(val):
+    build_plot(ax, int(slider.val))
+    fig.canvas.draw_idle()
+
+slider.on_changed(update)
+
+#
+
+salvar = input("Salvar? <S/n>   ")
+if salvar != "n" and salvar != "N":
+    np.save("labirinto.npy", labirinto)
+
+# p/ carregar depois
+# labirinto_loaded = np.load("labirinto.npy")
 
 plt.show()
+
